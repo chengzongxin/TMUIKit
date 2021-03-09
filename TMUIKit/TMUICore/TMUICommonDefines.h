d5784ccae3614f4462e67551021ceda44f94f8e2
Joe.cheng <joe.cheng@corp.to8to.com>
2021-03-09 18:20:44 +0800

    backup

--- TMUIKit/TMUICore/TMUICommonDefines.h
+++ TMUIKit/TMUICore/TMUICommonDefines.h
@@ -238,21 +238,6 @@
 
 #define CGSizeMax CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)
 
-#define UIImageMake(img) [UIImage imageNamed:img]
-
-/// 使用文件名(不带后缀名，仅限png)创建一个UIImage对象，不会被系统缓存，用于不被复用的图片，特别是大图
-#define UIImageMakeWithFile(name) UIImageMakeWithFileAndSuffix(name, @"png")
-#define UIImageMakeWithFileAndSuffix(name, suffix) [UIImage imageWithContentsOfFile:[NSString stringWithFormat:@"%@/%@.%@", [[NSBundle mainBundle] resourcePath], name, suffix]]
-
-/// 字体相关的宏，用于快速创建一个字体对象，更多创建宏可查看 UIFont+TMUI.h
-#define UIFontMake(size) [UIFont systemFontOfSize:size]
-#define UIFontItalicMake(size) [UIFont italicSystemFontOfSize:size] /// 斜体只对数字和字母有效，中文无效
-#define UIFontBoldMake(size) [UIFont boldSystemFontOfSize:size]
-#define UIFontBoldWithFont(_font) [UIFont boldSystemFontOfSize:_font.pointSize]
-
-/// UIColor 相关的宏，用于快速创建一个 UIColor 对象，更多创建的宏可查看 UIColor+TMUI.h
-#define UIColorMake(r, g, b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
-#define UIColorMakeWithRGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a/1.0]
 
 
 #pragma mark - 数学计算
@@ -291,24 +276,6 @@ AddAccessibilityHint(NSObject *obj, NSString *hint) {
  @param getter 目标 getter selector
  @return 对应的 setter selector
  */
-
-#define SETTER_METHOD1(getter) \
-{NSString *getterString = NSStringFromSelector(getter);\
-    NSMutableString *setterString = [[NSMutableString alloc] initWithString:@"set"];\
-    [setterString appendString:getterString.tmui_capitalizedString];\
-    [setterString appendString:@":"];\
-    SEL setter = NSSelectorFromString(setterString);\
-return setter;}\
-
-#define SETTER_METHOD(getter) \
-- (SEL)setterWithGetter1:(SEL)getter{\
-    NSString *getterString = NSStringFromSelector(getter);\
-    NSMutableString *setterString = [[NSMutableString alloc] initWithString:@"set"];\
-    [setterString appendString:getterString.tmui_capitalizedString];\
-    [setterString appendString:@":"];\
-    SEL setter = NSSelectorFromString(setterString);\
-    return setter;\
-}
 
 CG_INLINE SEL
 setterWithGetter(SEL getter) {
