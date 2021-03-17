800591244a23db8fe81f2cfe4cc35b63c2da6f41
Joe.cheng <joe.cheng@corp.to8to.com>
2021-03-17 09:13:15 +0800

    【更新】ignore files

--- Example/Pods/Target Support Files/Pods-TMUIKit_Tests/Pods-TMUIKit_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMUIKit_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_TMUIKit_Tests
-@end
--- Example/Pods/Target Support Files/TMUIKit/TMUIKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TMUIKit : NSObject
-@end
-@implementation PodsDummy_TMUIKit
-@end
