0488bf2e80d4d93a87f4a377fdcab2d87b0484ee
Joe.cheng <joe.cheng@corp.to8to.com>
2021-03-19 17:25:36 +0800

    update

--- Example/TMUIKit/Components/ChainUI/ChainUIViewController1.m
+++ Example/TMUIKit/Components/ChainUI/ChainUIViewController1.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     
     Color(0xFF990010);
-    id l1 = Label.str(@"this is title").styles(h1);
+    id l1 = Label.str(@"Color() 颜色宏的使用：").styles(h1);
     
     
     /*
@@ -41,14 +41,14 @@
     Log(CUI_TYPE(@"red,0.5"));
     
     VerStack(l1,
-             Label.str(@"[UIColor brownColor]").txtColor(Color([UIColor brownColor])), // UIExtendedSRGBColorSpace
-             Label.str(@"red,0.5").txtColor(Color(@"gray,0.5")),
-             Label.str(@"255,0,0,1").txtColor(Color(@"255,200,0,1")),
-             Label.str(@"#F00,0.5").txtColor(Color(@"#A00,0.5")),
-             Label.str(@"random,0.5").txtColor(Color(@"random,0.5")),
-             Label.str(@"0x00FFFF").txtColor(Color(@"0x00FFFF")),
-             Label.str(@"0x00FFFF").txtColor(Color(0x00FFFF50)),
-             Label.str(@"0x00FFFF").txtColor(Color(0x00FFFF)),
+             Label.str(@"颜色对象 [UIColor brownColor]").txtColor(Color([UIColor brownColor])), // UIExtendedSRGBColorSpace
+             Label.str(@"系统颜色 gray,0.5").txtColor(Color(@"gray,0.5")),
+             Label.str(@"RGBA字符串 255,200,0,1").txtColor(Color(@"255,200,0,1")),
+             Label.str(@"RGBA字符串 #A00,0.5").txtColor(Color(@"#A00,0.5")),
+             Label.str(@"随机色 random,0.5").txtColor(Color(@"random,0.5")),
+             Label.str(@"字符串  0x00FFFF").txtColor(Color(@"0x00FFFF")),
+             Label.str(@"十六进制（含alpha）0x00FFFF50").txtColor(Color(0x00FFFF50)),
+             Label.str(@"十六进制  0x00FFFF").txtColor(Color(0x00FFFF)),
              CUISpring)
     .embedIn(self.view, NavigationContentTop + 20,20,0).gap(10);
     
